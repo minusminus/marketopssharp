4680d5e5799a28d5e2655f67d5fc1d74b91c82a9
Michał Śmiechowski <Michal.Smiechowski@dsa.com.pl>
2019-06-04 17:00:55 +0200

    initialized SMA calculator

---
+++ MarketOps.Stats.Tests/SMATests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
+using MarketOps.Stats.Calculators;
+
+namespace MarketOps.Stats.Tests
+{
+    [TestFixture]
+    public class SMATests
+    {
+        private readonly SMA _testObj = new SMA();
+
+        [Test]
+        public void SMA__CalculatesCorrectly()
+        {
+            float[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            float[] expected = {2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f, 8.5f};
+
+            float[] res = _testObj.Calculate(data, 4);
+            res.ShouldBe(expected);
+        }
+    }
+}
---
+++ MarketOps.Stats/Calculators/SMA.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOps.Stats.Calculators
+{
+    /// <summary>
+    /// Calculates simple moving average on provided array
+    /// </summary>
+    public class SMA
+    {
+        private bool CanCalculate(float[] data, int period)
+        {
+            return data.Length >= period;
+        }
+
+        private float GetInitialSum(float[] data, int period)
+        {
+            return data.Take(period).Sum();
+            //float sum = 0;
+            //for (int i = 0; i < period; i++)
+            //    sum += data[i];
+            //return sum;
+        }
+
+        private float[] CalculateSMA(float[] data, int period)
+        {
+            float[] res = new float[data.Length - period + 1];
+            float currentSum = GetInitialSum(data, period);
+
+            for (int i = 0; i < res.Length - 1; i++)
+            {
+                res[i] = currentSum/(float) period;
+                currentSum -= data[i];
+                currentSum += data[i + period];
+            }
+            res[res.Length - 1] = currentSum / (float)period;
+
+            return res;
+        }
+
+        public float[] Calculate(float[] data, int period)
+        {
+            if (!CanCalculate(data, period)) return new float[0];
+            return CalculateSMA(data, period);
+        }
+    }
+}

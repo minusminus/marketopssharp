5cd02cc64c1fcc3f1b013ce565228f01895b75ec
minusminus <minusminus@tlen.pl>
2019-06-15 19:40:07 +0200

    - initialized stock stats

---
+++ MarketOps.Stats/Stats/StatSMA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MarketOps.StockData.Types;
+using MarketOps.Stats.Calculators;
+
+namespace MarketOps.Stats.Stats
+{
+    public class StatSMA : StockStat
+    {
+        public StatSMA()
+        {
+            _data = new float[1][];
+        }
+
+        public override void Calculate(StockPricesData data)
+        {
+            SMA calc = new SMA();
+            _data[0] = calc.Calculate(data.C, 20);
+        }
+    }
+}
---
+++ MarketOps.StockData/Types/StockStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MarketOps.StockData.Types
+{
+    /// <summary>
+    /// Statistic data for stock
+    /// </summary>
+    public abstract class StockStat
+    {
+        protected float[][] _data;
+
+        public int Count => _data.Length;
+        public float[] Data(int index) => _data[index];
+        public abstract void Calculate(StockPricesData data);
+    }
+}

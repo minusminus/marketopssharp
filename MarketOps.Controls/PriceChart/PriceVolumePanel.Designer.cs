6031f9399ac9a5380759ddc3f9c299440687f19b
Michał Śmiechowski <Michal.Smiechowski@dsa.com.pl>
2019-12-20 13:10:11 +0100

    - added icons to chart control

--- MarketOps.Controls/PriceChart/PriceVolumePanel.Designer.cs
+++ MarketOps.Controls/PriceChart/PriceVolumePanel.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PriceVolumePanel));
             this.pnlButtons = new System.Windows.Forms.Panel();
             this.btnMirrorChart = new System.Windows.Forms.CheckBox();
             this.btnDataRange = new System.Windows.Forms.Button();
@@ -43,6 +44,7 @@
             this.cmnChart = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.miStatsOnPriceChart = new System.Windows.Forms.ToolStripMenuItem();
             this.miAdditionalStats = new System.Windows.Forms.ToolStripMenuItem();
+            this.ilIcons = new System.Windows.Forms.ImageList(this.components);
             this.chartPV = new MarketOps.Controls.PriceChart.PriceVolumeChart();
             this.pnlButtons.SuspendLayout();
             this.pnlInfo.SuspendLayout();
@@ -67,36 +69,39 @@
             // 
             this.btnMirrorChart.Appearance = System.Windows.Forms.Appearance.Button;
             this.btnMirrorChart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMirrorChart.ImageIndex = 4;
+            this.btnMirrorChart.ImageList = this.ilIcons;
             this.btnMirrorChart.Location = new System.Drawing.Point(268, 4);
             this.btnMirrorChart.Name = "btnMirrorChart";
             this.btnMirrorChart.Size = new System.Drawing.Size(32, 24);
             this.btnMirrorChart.TabIndex = 7;
             this.btnMirrorChart.TabStop = false;
-            this.btnMirrorChart.Text = "Yrev";
             this.btnMirrorChart.UseVisualStyleBackColor = true;
             this.btnMirrorChart.CheckedChanged += new System.EventHandler(this.btnMirrorChart_CheckedChanged);
             // 
             // btnDataRange
             // 
             this.btnDataRange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDataRange.ImageIndex = 5;
+            this.btnDataRange.ImageList = this.ilIcons;
             this.btnDataRange.Location = new System.Drawing.Point(96, 4);
             this.btnDataRange.Name = "btnDataRange";
             this.btnDataRange.Size = new System.Drawing.Size(32, 24);
             this.btnDataRange.TabIndex = 6;
             this.btnDataRange.TabStop = false;
-            this.btnDataRange.Text = "range";
             this.btnDataRange.UseVisualStyleBackColor = true;
             this.btnDataRange.Click += new System.EventHandler(this.btnDataRange_Click);
             // 
             // btnPrependData
             // 
             this.btnPrependData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrependData.ImageIndex = 2;
+            this.btnPrependData.ImageList = this.ilIcons;
             this.btnPrependData.Location = new System.Drawing.Point(58, 4);
             this.btnPrependData.Name = "btnPrependData";
             this.btnPrependData.Size = new System.Drawing.Size(32, 24);
             this.btnPrependData.TabIndex = 5;
             this.btnPrependData.TabStop = false;
-            this.btnPrependData.Text = "prepend";
             this.btnPrependData.UseVisualStyleBackColor = true;
             this.btnPrependData.Click += new System.EventHandler(this.btnPrependData_Click);
             // 
@@ -104,12 +109,13 @@
             // 
             this.btnPriceChartCandle.Appearance = System.Windows.Forms.Appearance.Button;
             this.btnPriceChartCandle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPriceChartCandle.ImageIndex = 0;
+            this.btnPriceChartCandle.ImageList = this.ilIcons;
             this.btnPriceChartCandle.Location = new System.Drawing.Point(354, 4);
             this.btnPriceChartCandle.Name = "btnPriceChartCandle";
             this.btnPriceChartCandle.Size = new System.Drawing.Size(32, 24);
             this.btnPriceChartCandle.TabIndex = 4;
             this.btnPriceChartCandle.TabStop = false;
-            this.btnPriceChartCandle.Text = "C";
             this.btnPriceChartCandle.UseVisualStyleBackColor = true;
             this.btnPriceChartCandle.CheckedChanged += new System.EventHandler(this.btnPriceChartCandle_CheckedChanged);
             // 
@@ -117,24 +123,26 @@
             // 
             this.btnPriceChartLine.Appearance = System.Windows.Forms.Appearance.Button;
             this.btnPriceChartLine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPriceChartLine.ImageIndex = 1;
+            this.btnPriceChartLine.ImageList = this.ilIcons;
             this.btnPriceChartLine.Location = new System.Drawing.Point(316, 4);
             this.btnPriceChartLine.Name = "btnPriceChartLine";
             this.btnPriceChartLine.Size = new System.Drawing.Size(32, 24);
             this.btnPriceChartLine.TabIndex = 3;
             this.btnPriceChartLine.TabStop = false;
-            this.btnPriceChartLine.Text = "L";
             this.btnPriceChartLine.UseVisualStyleBackColor = true;
             this.btnPriceChartLine.CheckedChanged += new System.EventHandler(this.btnPriceChartLine_CheckedChanged);
             // 
             // btnRefresh
             // 
             this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.ImageIndex = 3;
+            this.btnRefresh.ImageList = this.ilIcons;
             this.btnRefresh.Location = new System.Drawing.Point(10, 4);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(32, 24);
             this.btnRefresh.TabIndex = 0;
             this.btnRefresh.TabStop = false;
-            this.btnRefresh.Text = "refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
@@ -186,7 +194,7 @@
             this.miAdditionalStats});
             this.cmnChart.Name = "cmnChart";
             this.cmnChart.ShowImageMargin = false;
-            this.cmnChart.Size = new System.Drawing.Size(144, 70);
+            this.cmnChart.Size = new System.Drawing.Size(144, 48);
             // 
             // miStatsOnPriceChart
             // 
@@ -199,6 +207,17 @@
             this.miAdditionalStats.Name = "miAdditionalStats";
             this.miAdditionalStats.Size = new System.Drawing.Size(143, 22);
             this.miAdditionalStats.Text = "Additional stats";
+            // 
+            // ilIcons
+            // 
+            this.ilIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ilIcons.ImageStream")));
+            this.ilIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.ilIcons.Images.SetKeyName(0, "candlestick-chart.png");
+            this.ilIcons.Images.SetKeyName(1, "line-chart.png");
+            this.ilIcons.Images.SetKeyName(2, "previous.png");
+            this.ilIcons.Images.SetKeyName(3, "refresh.png");
+            this.ilIcons.Images.SetKeyName(4, "revert-hand-drawn-arrows2.png");
+            this.ilIcons.Images.SetKeyName(5, "table-selection.png");
             // 
             // chartPV
             // 
@@ -243,5 +262,6 @@
         private System.Windows.Forms.ContextMenuStrip cmnChart;
         private System.Windows.Forms.ToolStripMenuItem miStatsOnPriceChart;
         private System.Windows.Forms.ToolStripMenuItem miAdditionalStats;
+        private System.Windows.Forms.ImageList ilIcons;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # marketopssharp
 market ops tools (sharp version)
+
+
+Icons made by:
+- Freepik from www.flaticon.com
+- Gregor Cresnar from www.flaticon.com
+- Vectors Market from www.flaticon.com
+

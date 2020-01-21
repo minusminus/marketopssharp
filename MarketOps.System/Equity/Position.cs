7964c571e2c73a6f85527becf67b322829ff7b40
michal.smiechowski <michal.smiechowski@PC-MSM-W7>
2020-01-21 12:03:19 +0100

    - initialized position management data

---
+++ MarketOps.System/Equity/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MarketOps.StockData.Types;
+
+namespace MarketOps.System.Equity
+{
+    /// <summary>
+    /// Position data.
+    /// </summary>
+    internal struct Position
+    {
+        public StockDefinition Stock;
+        public DateTime TSOpen;
+        public DateTime TSClose;
+        public float Open;
+        public float Close;
+    }
+}
---
+++ MarketOps.System/Equity/Positions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOps.System.Equity
+{
+    /// <summary>
+    /// System positions data.
+    /// </summary>
+    internal class Positions
+    {
+        public readonly List<Position> Active = new List<Position>();
+        public readonly List<Position> Closed = new List<Position>();
+    }
+}

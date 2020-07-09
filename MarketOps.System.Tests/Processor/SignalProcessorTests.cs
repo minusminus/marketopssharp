74b78792aeb8d816d043988461151f172c150314
minusminus <minusminus@tlen.pl>
2020-07-09 20:55:13 +0200

    - extracted signals processor with first test

---
+++ MarketOps.System.Tests/Processor/SignalProcessorTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using NSubstitute;
+using MarketOps.System.Processor;
+using MarketOps.StockData.Types;
+using System;
+using System.Collections.Generic;
+using MarketOps.StockData.Interfaces;
+using MarketOps.System.Interfaces;
+using MarketOps.System.Tests.Mocks;
+
+namespace MarketOps.System.Tests.Processor
+{
+    [TestFixture]
+    public class SignalProcessorTests
+    {
+        private readonly DateTime LastDate = DateTime.Now.Date;
+        private readonly int PricesCount = 10;
+
+        private SignalProcessor TestObj;
+        private IDataLoader _dataLoader;
+        private StockPricesData _pricesData;
+
+        private bool _signalSelectorCalled;
+        private bool _openPriceLevelCalled;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dataLoader = Substitute.For<IDataLoader>();
+            TestObj = new SignalProcessor(_dataLoader);
+            _signalSelectorCalled = false;
+            _openPriceLevelCalled = false;
+
+            _pricesData = new StockPricesData(PricesCount);
+            for (int i = 0; i < _pricesData.Length; i++)
+            {
+                _pricesData.O[i] = i;
+                _pricesData.H[i] = i;
+                _pricesData.L[i] = i;
+                _pricesData.C[i] = i;
+                _pricesData.TS[i] = LastDate.AddDays(-_pricesData.Length + i + 1);
+            }
+            _dataLoader.Get(default, default, default, default, default).ReturnsForAnyArgs(_pricesData);
+        }
+
+        [Test]
+        public void Process_EmptySignalsList__DoesNothing()
+        {
+            List<Signal> signals = new List<Signal>();
+            SystemEquity equity = new SystemEquity();
+            TestObj.Process(signals, LastDate, equity,
+                (_) => { _signalSelectorCalled = true; return new List<Signal>() { new Signal() }; },
+                (_, __, ___) => { _openPriceLevelCalled = true; return -1; });
+            _signalSelectorCalled.ShouldBeFalse();
+            _openPriceLevelCalled.ShouldBeFalse();
+            signals.Count.ShouldBe(0);
+            equity.PositionsActive.Count.ShouldBe(0);
+            equity.PositionsClosed.Count.ShouldBe(0);
+            equity.ClosedPositionsValue.Count.ShouldBe(0);
+            equity.Value.Count.ShouldBe(0);
+        }
+    }
+}
---
+++ MarketOps.System/Processor/SignalProcessor.cs
@@ -0,0 +1,56 @@
+﻿using MarketOps.StockData.Extensions;
+using MarketOps.StockData.Types;
+using MarketOps.System.Extensions;
+using MarketOps.System.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace MarketOps.System.Processor
+{
+    /// <summary>
+    /// System signal processor.
+    /// </summary>
+    internal class SignalProcessor
+    {
+        private readonly IDataLoader _dataLoader;
+
+        public SignalProcessor(IDataLoader dataLoader)
+        {
+            _dataLoader = dataLoader;
+        }
+
+        public void Process(List<Signal> signals, DateTime ts, SystemEquity equity, 
+            Func<IEnumerable<Signal>, IEnumerable<Signal>> signalsSelector,
+            Func<StockPricesData, int, Signal, float> openPriceLevel)
+        {
+            if (signals.Count == 0) return;
+
+            //var signalsToProcess = new HashSet<Signal>(signals.Where(s => s.Type == SignalType.EnterOnOpen));
+            var signalsToProcess = new HashSet<Signal>(signalsSelector(signals));
+
+            foreach (Signal signal in signalsToProcess)
+            {
+                StockPricesData pricesData = _dataLoader.Get(signal.Stock.Name, signal.DataRange, signal.IntradayInterval, ts, ts);
+                int pricesDataIndex = pricesData.FindByTS(ts);
+
+                if (signal.ReversePosition)
+                {
+                    PositionDir newPosDir = signal.Direction;
+                    int currPos = equity.PositionsActive.FindIndex(p => p.Stock.ID == signal.Stock.ID);
+                    if (currPos > -1)
+                    {
+                        newPosDir = equity.PositionsActive[currPos].Direction;
+                        equity.Close(currPos, ts, pricesData.O[pricesDataIndex]);
+                    }
+                    //equity.Open(signal.Stock, newPosDir, ts, pricesData.O[pricesDataIndex], signal.Volume, signal.DataRange, signal.IntradayInterval);
+                    equity.Open(signal.Stock, newPosDir, ts, openPriceLevel(pricesData, pricesDataIndex, signal), signal.Volume, signal.DataRange, signal.IntradayInterval);
+                }
+                else
+                    //equity.Open(signal.Stock, signal.Direction, ts, pricesData.O[pricesDataIndex], signal.Volume, signal.DataRange, signal.IntradayInterval);
+                    equity.Open(signal.Stock, signal.Direction, ts, openPriceLevel(pricesData, pricesDataIndex, signal), signal.Volume, signal.DataRange, signal.IntradayInterval);
+            }
+
+            signals.RemoveAll(s => signalsToProcess.Contains(s));
+        }
+    }
+}

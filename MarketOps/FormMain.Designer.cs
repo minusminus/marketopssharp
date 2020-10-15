01841dc4ba21850be4db364f8fc387c3186096f4
minusminus <minusminus@tlen.pl>
2020-10-15 21:29:12 +0200

    - added main tabcontrol with charts and simulation tabs

--- MarketOps/FormMain.Designer.cs
+++ MarketOps/FormMain.Designer.cs
@@ -33,15 +33,20 @@
             this.menuMain = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.dataPumpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGenerationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tcCharts = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlCharts = new System.Windows.Forms.Panel();
             this.cbStockDataRange = new System.Windows.Forms.ComboBox();
             this.edtStockName = new System.Windows.Forms.TextBox();
-            this.dataGenerationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tcMain = new System.Windows.Forms.TabControl();
+            this.tabCharts = new System.Windows.Forms.TabPage();
+            this.tabSimulation = new System.Windows.Forms.TabPage();
             this.menuMain.SuspendLayout();
             this.tcCharts.SuspendLayout();
-            this.pnlMain.SuspendLayout();
+            this.pnlCharts.SuspendLayout();
+            this.tcMain.SuspendLayout();
+            this.tabCharts.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnLoad
@@ -82,26 +87,33 @@
             this.dataPumpToolStripMenuItem,
             this.dataGenerationToolStripMenuItem});
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(42, 20);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
             this.toolStripMenuItem1.Text = "Data";
             // 
             // dataPumpToolStripMenuItem
             // 
             this.dataPumpToolStripMenuItem.Name = "dataPumpToolStripMenuItem";
-            this.dataPumpToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.dataPumpToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
             this.dataPumpToolStripMenuItem.Text = "Data pump";
             this.dataPumpToolStripMenuItem.Click += new System.EventHandler(this.dataPumpToolStripMenuItem_Click);
+            // 
+            // dataGenerationToolStripMenuItem
+            // 
+            this.dataGenerationToolStripMenuItem.Name = "dataGenerationToolStripMenuItem";
+            this.dataGenerationToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.dataGenerationToolStripMenuItem.Text = "Data generation";
+            this.dataGenerationToolStripMenuItem.Click += new System.EventHandler(this.dataGenerationToolStripMenuItem_Click);
             // 
             // tcCharts
             // 
             this.tcCharts.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
             this.tcCharts.Controls.Add(this.tabPage1);
             this.tcCharts.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tcCharts.Location = new System.Drawing.Point(195, 24);
+            this.tcCharts.Location = new System.Drawing.Point(195, 0);
             this.tcCharts.Multiline = true;
             this.tcCharts.Name = "tcCharts";
             this.tcCharts.SelectedIndex = 0;
-            this.tcCharts.Size = new System.Drawing.Size(758, 509);
+            this.tcCharts.Size = new System.Drawing.Size(750, 474);
             this.tcCharts.TabIndex = 10;
             this.tcCharts.TabStop = false;
             this.tcCharts.Deselected += new System.Windows.Forms.TabControlEventHandler(this.tcCharts_Deselected);
@@ -113,23 +125,23 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(750, 480);
+            this.tabPage1.Size = new System.Drawing.Size(742, 445);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // pnlMain
+            // pnlCharts
             // 
-            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlMain.Controls.Add(this.cbStockDataRange);
-            this.pnlMain.Controls.Add(this.edtStockName);
-            this.pnlMain.Controls.Add(this.btnLoad);
-            this.pnlMain.Controls.Add(this.button1);
-            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlMain.Location = new System.Drawing.Point(0, 24);
-            this.pnlMain.Name = "pnlMain";
-            this.pnlMain.Size = new System.Drawing.Size(195, 509);
-            this.pnlMain.TabIndex = 0;
+            this.pnlCharts.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCharts.Controls.Add(this.cbStockDataRange);
+            this.pnlCharts.Controls.Add(this.edtStockName);
+            this.pnlCharts.Controls.Add(this.btnLoad);
+            this.pnlCharts.Controls.Add(this.button1);
+            this.pnlCharts.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlCharts.Location = new System.Drawing.Point(0, 0);
+            this.pnlCharts.Name = "pnlCharts";
+            this.pnlCharts.Size = new System.Drawing.Size(195, 474);
+            this.pnlCharts.TabIndex = 0;
             // 
             // cbStockDataRange
             // 
@@ -148,20 +160,46 @@
             this.edtStockName.TabIndex = 1;
             this.edtStockName.Text = "WIG";
             // 
-            // dataGenerationToolStripMenuItem
+            // tcMain
             // 
-            this.dataGenerationToolStripMenuItem.Name = "dataGenerationToolStripMenuItem";
-            this.dataGenerationToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.dataGenerationToolStripMenuItem.Text = "Data generation";
-            this.dataGenerationToolStripMenuItem.Click += new System.EventHandler(this.dataGenerationToolStripMenuItem_Click);
+            this.tcMain.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
+            this.tcMain.Controls.Add(this.tabCharts);
+            this.tcMain.Controls.Add(this.tabSimulation);
+            this.tcMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcMain.Location = new System.Drawing.Point(0, 24);
+            this.tcMain.Name = "tcMain";
+            this.tcMain.Padding = new System.Drawing.Point(20, 6);
+            this.tcMain.SelectedIndex = 0;
+            this.tcMain.Size = new System.Drawing.Size(953, 509);
+            this.tcMain.TabIndex = 0;
+            this.tcMain.TabStop = false;
+            // 
+            // tabCharts
+            // 
+            this.tabCharts.Controls.Add(this.tcCharts);
+            this.tabCharts.Controls.Add(this.pnlCharts);
+            this.tabCharts.Location = new System.Drawing.Point(4, 31);
+            this.tabCharts.Name = "tabCharts";
+            this.tabCharts.Size = new System.Drawing.Size(945, 474);
+            this.tabCharts.TabIndex = 0;
+            this.tabCharts.Text = "Charts";
+            this.tabCharts.UseVisualStyleBackColor = true;
+            // 
+            // tabSimulation
+            // 
+            this.tabSimulation.Location = new System.Drawing.Point(4, 31);
+            this.tabSimulation.Name = "tabSimulation";
+            this.tabSimulation.Size = new System.Drawing.Size(945, 474);
+            this.tabSimulation.TabIndex = 1;
+            this.tabSimulation.Text = "Simulation";
+            this.tabSimulation.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(953, 533);
-            this.Controls.Add(this.tcCharts);
-            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.tcMain);
             this.Controls.Add(this.menuMain);
             this.MainMenuStrip = this.menuMain;
             this.Name = "FormMain";
@@ -170,8 +208,10 @@
             this.menuMain.ResumeLayout(false);
             this.menuMain.PerformLayout();
             this.tcCharts.ResumeLayout(false);
-            this.pnlMain.ResumeLayout(false);
-            this.pnlMain.PerformLayout();
+            this.pnlCharts.ResumeLayout(false);
+            this.pnlCharts.PerformLayout();
+            this.tcMain.ResumeLayout(false);
+            this.tabCharts.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,10 +225,13 @@
         private System.Windows.Forms.ToolStripMenuItem dataPumpToolStripMenuItem;
         private System.Windows.Forms.TabControl tcCharts;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlCharts;
         private System.Windows.Forms.TextBox edtStockName;
         private System.Windows.Forms.ComboBox cbStockDataRange;
         private System.Windows.Forms.ToolStripMenuItem dataGenerationToolStripMenuItem;
+        private System.Windows.Forms.TabControl tcMain;
+        private System.Windows.Forms.TabPage tabCharts;
+        private System.Windows.Forms.TabPage tabSimulation;
     }
 }
 

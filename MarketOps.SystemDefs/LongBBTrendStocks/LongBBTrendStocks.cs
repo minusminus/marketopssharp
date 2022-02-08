1aeb8bf9688614935699e140ca236304703b6c7f
minusminus <minusminus@tlen.pl>
2022-02-08 17:08:23 +0100

    initialized LongBBTrendStocks

---
+++ MarketOps.SystemDefs/LongBBTrendStocks/LongBBTrendStocks.cs
@@ -0,0 +1,49 @@
+﻿using MarketOps.StockData.Extensions;
+using MarketOps.StockData.Interfaces;
+using MarketOps.SystemData.Interfaces;
+using MarketOps.SystemData.Types;
+using MarketOps.SystemExecutor.MM;
+
+namespace MarketOps.SystemDefs.LongBBTrendStocks
+{
+    /// <summary>
+    /// Catching long trends on stocks (on monthly data) with BB(5, 2)
+    /// </summary>
+    public class LongBBTrendStocks : SystemDefinition
+    {
+        private readonly IStockDataProvider _dataProvider;
+        private readonly ISystemDataLoader _dataLoader;
+
+        public LongBBTrendStocks(IStockDataProvider dataProvider, ISystemDataLoader dataLoader,
+            ISlippage slippage, ICommission commission, ISystemExecutionLogger systemExecutionLogger)
+        {
+            _dataProvider = dataProvider;
+            _dataLoader = dataLoader;
+            _slippage = slippage;
+            _commission = commission;
+            _systemExecutionLogger = systemExecutionLogger;
+
+            SystemParams.Set(LongBBTrendStocksParams.StockName, "");
+            SystemParams.Set(LongBBTrendStocksParams.BBPeriod, 5);
+            SystemParams.Set(LongBBTrendStocksParams.BBSigmaWidth, 2f);
+        }
+
+        public override void Prepare()
+        {
+            SignalsLongBBTrendStocks signals = new SignalsLongBBTrendStocks(
+                SystemParams.Get(LongBBTrendStocksParams.StockName).As<string>(),
+                StockData.Types.StockDataRange.Monthly,
+                SystemParams.Get(LongBBTrendStocksParams.BBPeriod).As<int>(),
+                SystemParams.Get(LongBBTrendStocksParams.BBSigmaWidth).As<float>(),
+                _dataLoader, _dataProvider,
+                new MMSignalVolumeForAllCash(_commission));
+
+            _dataDefinitionProvider = signals;
+            _signalGeneratorOnOpen = null;
+            _signalGeneratorOnClose = signals;
+            //_commission = null;
+            //_slippage = null;
+            _mmPositionCloseCalculator = null;
+        }
+    }
+}
---
+++ MarketOps.SystemDefs/LongBBTrendStocks/LongBBTrendStocksParams.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketOps.SystemDefs.LongBBTrendStocks
+{
+    public static class LongBBTrendStocksParams
+    {
+        public static readonly string StockName = "StockName";
+        public static readonly string BBPeriod = "BB period";
+        public static readonly string BBSigmaWidth = "BB sigma width";
+    }
+}

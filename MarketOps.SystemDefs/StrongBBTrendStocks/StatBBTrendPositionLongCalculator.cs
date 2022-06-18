785581eb4d92b95d5992de5c6e256703c6210429
minusminus <minusminus@tlen.pl>
2022-06-18 12:44:58 +0200

    - first implementation of StatBBTrendPositionLong

--- MarketOps.SystemDefs/StrongBBTrendStocks/StatBBTrendPositionLongCalculator.cs
+++ MarketOps.SystemDefs/StrongBBTrendStocks/StatBBTrendPositionLongCalculator.cs
@@ -5,7 +5,7 @@ using System;
 namespace MarketOps.SystemDefs.StrongBBTrendStocks
 {
     /// <summary>
-    /// Calculates strong trend (on monthly data).
+    /// Calculates strong trend (on monthly data) for current tick based on previous tick data.
     /// Trend starts tick after BB breakout.
     /// Trend ends tick after trailing stop breakout.
     /// </summary>
@@ -16,29 +16,32 @@ namespace MarketOps.SystemDefs.StrongBBTrendStocks
 
         public static float[] Calculate(float[] dataC, float[] dataL, int bbPeriod, float bbSigmaWidth, int trailingStopMinOfN)
         {
+            if (!CanCalculate(dataC.Length, bbPeriod, trailingStopMinOfN)) return new float[0];
+
             var bbData = BB.Calculate(dataC, bbPeriod, bbSigmaWidth);
             var hlData = HLChannel.Calculate(dataL, dataL, trailingStopMinOfN);
-
             return CalculateData(dataC, dataL, bbData, hlData, bbPeriod, trailingStopMinOfN);
         }
+
+        private static bool CanCalculate(int dataLength, int bbPeriod, int trailingStopMinOfN) =>
+            dataLength >= Math.Max(bbPeriod, trailingStopMinOfN);
 
         private static float[] CalculateData(float[] dataC, float[] dataL, BBData bbData, HLChannelData hlData, int bbPeriod, int trailingStopMinOfN)
         {
             LongBBTrendInfo trendInfo = new LongBBTrendInfo();
             int startIndex = Math.Max(bbPeriod, trailingStopMinOfN);
-            float[] result = new float[dataC.Length - startIndex + 1];
-            result[0] = NoTrend;
-            for (int i = startIndex + 1; i < dataC.Length; i++)
+            float[] result = new float[dataC.Length - startIndex];
+            for (int i = startIndex; i < dataC.Length; i++)
             {
                 trendInfo.CurrentTrend = BBTrendRecognizer.BBTrendRecognizer.RecognizeTrendOnC(dataC, bbData.BBL, bbData.BBH, bbPeriod, i - 1, trendInfo.CurrentTrend, out _, ref trendInfo.CurrentTrendStartIndex);
 
-                if (result[i - 1] == NoTrend)
+                if ((i == startIndex) || (result[i - startIndex - 1] == NoTrend))
                 {
                     BBTrendExpectation expectation = BBTrendRecognizer.BBTrendRecognizer.GetExpectation(dataC, bbData.SMA, bbPeriod, i - 1, trendInfo.CurrentTrend);
-                    result[i] = (expectation == BBTrendExpectation.UpAndRaising) ? InTrend : NoTrend;
+                    result[i - startIndex] = (expectation == BBTrendExpectation.UpAndRaising) ? InTrend : NoTrend;
                 }
                 else
-                    result[i] = (hlData.L[i - 1 - trailingStopMinOfN] <= dataL[i - 1]) ? NoTrend : InTrend;
+                    result[i - startIndex] = (hlData.L[i - 1 - trailingStopMinOfN] <= dataL[i - 1]) ? NoTrend : InTrend;
             }
             return result;
         }

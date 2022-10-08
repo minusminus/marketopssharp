e6f63473d64de51f3f97417fcd9f3e31e6a0648a
minusminus <minusminus@tlen.pl>
2022-10-08 14:27:30 +0200

    - version raised to 1.2
    - initialized EqualRiskPositionsBalancer

---
+++ MarketOps.SystemExecutor/PositionsBalance/EqualRiskPositionsBalancer.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System;
+
+namespace MarketOps.SystemExecutor.PositionsBalance
+{
+    /// <summary>
+    /// Calculates positions balance based on provided expected risk.
+    /// All positions will have equal risk after balance.
+    /// Each position size will be no more than provided weight.
+    /// Weights should sum to <= 1.
+    /// </summary>
+    public static class EqualRiskPositionsBalancer
+    {
+        public static float[] CalculateBalanceEqualWeights(float[] expectedRisks, float[] prices, float equityValue) =>
+            CalculateBalance(expectedRisks, Enumerable.Repeat(1f / expectedRisks.Length, expectedRisks.Length).ToArray(), prices, equityValue);
+
+        public static float[] CalculateBalance(float[] expectedRisks, float[] positionsWeights, float[] prices, float equityValue)
+        {
+            CheckEqualLengths(expectedRisks, positionsWeights, prices);
+
+            float[] weightedRisks = CalculateWeightedRisks(expectedRisks, positionsWeights, equityValue);
+            float[] balance = CalculateInitialBalance(weightedRisks);
+            return AdjustBalanceToEquityValue(balance, prices, CalculateWeightedEquityValue(positionsWeights, equityValue));
+        }
+
+        private static void CheckEqualLengths(float[] expectedRisks, float[] positionsWeights, float[] prices)
+        {
+            if (expectedRisks.Length == 0)
+                throw new ArgumentException("Zero length tables");
+            if (expectedRisks.Length != positionsWeights.Length
+                || expectedRisks.Length != prices.Length)
+                throw new ArgumentException("Tables lengths differ");
+        }
+
+        private static float[] CalculateWeightedRisks(float[] expectedRisks, float[] positionsWeights, float equityValue)
+        {
+            float[] result = new float[expectedRisks.Length];
+
+            for (int i = 0; i < expectedRisks.Length; i++)
+                result[i] = positionsWeights[i] * expectedRisks[i] / equityValue;
+
+            return result;
+        }
+
+        private static float[] CalculateInitialBalance(float[] weightedRisks)
+        {
+            float[] result = new float[weightedRisks.Length];
+
+            float maxWeightedRisk = weightedRisks.Max();
+            for (int i = 0; i < weightedRisks.Length; i++)
+                result[i] = maxWeightedRisk / weightedRisks[i];
+
+            return result;
+        }
+
+        private static float CalculateWeightedEquityValue(float[] positionsWeights, float equityValue) =>
+            positionsWeights.Sum() * equityValue;
+
+        private static float[] AdjustBalanceToEquityValue(float[] balance, float[] prices, float weightedEquityValue)
+        {
+            float sum = 0;
+            for (int i = 0; i < balance.Length; i++)
+                sum += balance[i] * prices[i];
+            float balanceMultiplier = weightedEquityValue / sum;
+
+            for (int i = 0; i < balance.Length; i++)
+                balance[i] *= balanceMultiplier;
+
+            return balance;
+        }
+    }
+}
---
+++ MarketOps.Tests/SystemExecutor/PositionsBalance/EqualRiskPositionsBalancerTests.cs
@@ -0,0 +1,34 @@
+﻿using MarketOps.SystemExecutor.PositionsBalance;
+using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace MarketOps.Tests.SystemExecutor.PositionsBalance
+{
+    [TestFixture]
+    public class EqualRiskPositionsBalancerTests
+    {
+        [TestCase(new float[1] { 1f }, new float[1] { 1f }, new float[1] { 10f }, 100f, new float[1] { 10f })]
+        [TestCase(new float[2] { 1f, 1f }, new float[2] { 0.5f, 0.5f }, new float[2] { 10f, 10f }, 100f, new float[2] { 5f, 5f })]
+        [TestCase(new float[2] { 1f, 0.5f }, new float[2] { 0.5f, 0.5f }, new float[2] { 10f, 10f }, 100f, new float[2] { 10f * 1f / 3f, 10f * 2f / 3f })]
+        [TestCase(new float[2] { 1f, 1f }, new float[2] { 0.5f, 0.25f }, new float[2] { 10f, 10f }, 100f, new float[2] { 5f, 2.5f })]
+        [TestCase(new float[3] { 1f, 1f, 1f }, new float[3] { 1f / 3f, 1f / 3f, 1f / 3f }, new float[3] { 10f, 10f, 10f }, 100f, new float[3] { 10f / 3f, 10f / 3f, 10f / 3f })]
+        [TestCase(new float[3] { 1f, 0.5f, 1f }, new float[3] { 1f / 3f, 1f / 3f, 1f / 3f }, new float[3] { 10f, 10f, 10f }, 100f, new float[3] { 10f * 1f / 4f, 10f * 2f / 4f, 10f * 1f / 4f })]
+        public void CalculateBalance__CalculatesCorrectly(float[] expectedRisks, float[] positionsWeights, float[] prices, float equityValue, float[] expected)
+        {
+            EqualRiskPositionsBalancer.CalculateBalance(expectedRisks, positionsWeights, prices, equityValue).ShouldBe(expected, 0.01);
+        }
+
+        [Test]
+        public void CalculateBalance_ZeroLengths__Throws()
+        {
+            Should.Throw<ArgumentException>(() => EqualRiskPositionsBalancer.CalculateBalance(new float[0], new float[0], new float[0], 0));
+        }
+
+        [Test]
+        public void CalculateBalance_DifferentLengths__Throws()
+        {
+            Should.Throw<ArgumentException>(() => EqualRiskPositionsBalancer.CalculateBalance(new float[1], new float[2], new float[3], 0));
+        }
+    }
+}

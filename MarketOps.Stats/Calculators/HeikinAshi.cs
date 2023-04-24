524e2594f84c851c529ad9b1e728ce8308c0f937
minusminus <minusminus@tlen.pl>
2023-04-24 14:27:01 +0200

    - added HeikinAshi calculator with tests

---
+++ MarketOps.Stats/Calculators/HeikinAshi.cs
@@ -0,0 +1,35 @@
+﻿using MarketOps.StockData.Types;
+using System;
+
+namespace MarketOps.Stats.Calculators
+{
+    /// <summary>
+    /// Heikin-Ashi calculator.
+    /// </summary>
+    public static class HeikinAshi
+    {
+        public static HeikinAshiData Calculate(in StockPricesData data) => 
+            CanCalculate(data)
+                ? CalcualteData(data)
+                : new HeikinAshiData(0);
+
+        private static bool CanCalculate(in StockPricesData data) => 
+            (data.Length > 1);
+
+        private static HeikinAshiData CalcualteData(in StockPricesData data)
+        {
+            var result = new HeikinAshiData(data.Length - 1);
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                result.C[i - 1] = (data.O[i] + data.H[i] + data.L[i] + data.C[i]) / 4f;
+                result.O[i - 1] = (data.O[i - 1] + data.C[i - 1]) / 2f;
+                result.H[i - 1] = Math.Max(data.H[i], Math.Max(result.O[i - 1], result.C[i - 1]));
+                result.L[i - 1] = Math.Min(data.L[i], Math.Min(result.O[i - 1], result.C[i - 1]));
+                result.TS[i - 1] = data.TS[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MarketOps.Tests/Stats/Calculators/HeikinAshiTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using MarketOps.Stats.Calculators;
+using MarketOps.StockData.Types;
+using System;
+
+namespace MarketOps.Tests.Stats.Calculators
+{
+    [TestFixture]
+    public class HeikinAshiTests
+    {
+        [Test]
+        public void Calculate__ReturnsCorrectly()
+        {
+            var dt = DateTime.Now.Date;
+            var data = new StockPricesData(5);
+            FillDataArray(data.O, 10, 20, 30, 40, 50);
+            FillDataArray(data.H, 20, 30, 40, 50, 60);
+            FillDataArray(data.L, 5, 15, 25, 35, 45);
+            FillDataArray(data.C, 15, 25, 35, 45, 55);
+            FillDataArray(data.TS, dt);
+
+            var result = HeikinAshi.Calculate(data);
+
+            CheckLength(result, data.Length - 1);
+            result.O.ShouldBe(new float[] { 12.5f, 22.5f, 32.5f, 42.5f });
+            result.H.ShouldBe(new float[] { 30, 40, 50, 60 });
+            result.L.ShouldBe(new float[] { 12.5f, 22.5f, 32.5f, 42.5f });
+            result.C.ShouldBe(new float[] { 22.5f, 32.5f, 42.5f, 52.5f });
+            result.TS.ShouldBe(new DateTime[] { dt.AddDays(1), dt.AddDays(2), dt.AddDays(3), dt.AddDays(4) });
+        }
+
+        [Test]
+        public void Calculate_TooShortData__ReturnsEmpty([Values(0, 1)] int length)
+        {
+            var data = new StockPricesData(length);
+
+            var result = HeikinAshi.Calculate(data);
+
+            CheckLength(result, 0);
+        }
+
+        private static void CheckLength(HeikinAshiData result, int expectedLength)
+        {
+            result.O.Length.ShouldBe(expectedLength);
+            result.H.Length.ShouldBe(expectedLength);
+            result.L.Length.ShouldBe(expectedLength);
+            result.C.Length.ShouldBe(expectedLength);
+            result.TS.Length.ShouldBe(expectedLength);
+        }
+
+        private static void FillDataArray(in float[] data, params float[] values)
+        {
+            values.Length.ShouldBe(data.Length);
+            for (int i = 0; i < data.Length; i++)
+                data[i] = values[i];
+        }
+
+        private static void FillDataArray(in DateTime[] data, DateTime startDt)
+        {
+            for (int i = 0; i < data.Length; i++)
+                data[i] = startDt.AddDays(i);
+        }
+    }
+}
